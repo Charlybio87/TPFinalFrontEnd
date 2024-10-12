e5af3a57814d69bc2d533f70ebb38b15cebc438c
Carlos A. Ribas <ribascarlos.a@outlook.com>
2024-10-12 12:18:02 -0300

    Limpieza cache y re-instalacion de las dependencias

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
 import { Cursos, Home } from "./Pages"
-import './style/css/App.css'
+import '../src/App.css'
 
 function App() {
   

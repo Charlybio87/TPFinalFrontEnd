becc86b78fb12e41d1c338edc0b4e935f084d62a
Carlos A. Ribas <ribascarlos.a@outlook.com>
2024-10-12 11:40:01 -0300

    pendiente-> formulario de seleccion de cursos

--- src/Pages/Cursos/Cursos.jsx
+++ src/Pages/Cursos/Cursos.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Footer, Header } from '../../Components/Home'
 import useProducts from '../../hooks/useProducts'
+import '../../style/css/curses.css'
 
 const Cursos = () => {
   
@@ -11,8 +12,8 @@ const Cursos = () => {
   return (
     <>
       <div>
-        {/* <Header/> */}
-        <main >
+        <Header/>
+        <main className='container-curses'>
         {
           objeto.isLoading
             ? <h2>Cargando</h2>
@@ -20,18 +21,18 @@ const Cursos = () => {
               {
                 objeto.products.map((producto)=>{
                   return (
-                    <div key={producto.id}>
-                    <h3>{producto.titulo}</h3>
+                    <div key={producto.id} className='titulo-curses'>
+                    <h3 className='style-titulo'>{producto.titulo}</h3>
                     <div>
                       {
-                        <div>
+                        <div className='flex-curses'>
                         {
                           producto.curses.map( (curse)=>{
                           return (
-                            <div key={curse.id}>
-                              <div>
-                                <img src={curse.img} alt={curse.img} />
-                                <span>{curse.nombre}</span>
+                            <div key={curse.id}  className='curse-container'>
+                              <div className='img-container'>
+                                <img src={curse.img} alt={curse.img} className='styled-img'/>
+                                <p className='styled-p'>{curse.nombre} – <strong>{curse.precio}</strong></p>
                               </div>
                             </div>
                             )
